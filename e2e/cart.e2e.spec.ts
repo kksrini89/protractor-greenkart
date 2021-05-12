e773595b4f63a83abcd21f8dc1cd3b7ee873f888
srinivasan.k.k <srinivasan.k.k@kdi.kongsberg.com>
2021-05-12 16:07:25 +0530

    issue is resolved with 2nd test case.

--- e2e/cart.e2e.spec.ts
+++ e2e/cart.e2e.spec.ts
@@ -1,8 +1,8 @@
-import { browser, by, element } from 'protractor';
+import { $, browser, by, element, ElementFinder } from 'protractor';
 
 import { CartPageObject } from './../po/cart.po';
 
-describe('GreenKart - Add to cart', () => {
+describe('GreenKart - Add to cart:', () => {
   let cartPO: CartPageObject;
   beforeEach(() => {
     cartPO = new CartPageObject();
@@ -10,7 +10,7 @@ describe('GreenKart - Add to cart', () => {
   afterEach(() => {
     cartPO = null;
   });
-  it('Items Count Check: add item to cart', async () => {
+  it('Items Count Check', async () => {
     browser.waitForAngularEnabled(false);
     //   Navigate to 'https://rahulshettyacademy.com/seleniumPractise/#/'
     await cartPO.navigateTo('https://rahulshettyacademy.com/seleniumPractise/#/');
@@ -36,31 +36,53 @@ describe('GreenKart - Add to cart', () => {
     expect(totalProductsCount).toBe(parseInt(itemsCountEl));
   });
 
-  it('Price Check: add item to cart', async () => {
-    browser.waitForAngularEnabled(false);
+  it('Price Check', async () => {
+    await browser.waitForAngularEnabled(false);
     await cartPO.navigateTo('https://rahulshettyacademy.com/seleniumPractise/#/');
 
-    await browser.sleep(1000);
+    // await browser.sleep(1000);
+    await browser.executeScript("document.querySelector('.products-wrapper').scrollIntoView({ behavior: 'auto' })");
+    // document.querySelector('.products-wrapper').scrollIntoView({ behavior: 'auto' });
     //   Query the list items
     // const totalProductsCount = await cartPO.products.count();
     let totalPrice: number = 0;
 
-    element(by.className('products'))
-      .all(by.css('.product'))
-      .each(async (item, index) => {
-        await browser.sleep(2000);
-        item
-          .element(by.css("div[class='product-action'] button"))
-          .click()
-          .then(async () => {
-            item
-              .element(by.css("p[class='product-price']"))
-              .getText()
-              .then((val) => {
-                totalPrice += parseInt(val);
-              });
-          });
-      });
+    (await element.all(by.css('.product-action button'))).forEach(async (el: ElementFinder) => {
+      await browser.actions().mouseMove(el).click().perform();
+    });
+
+    await browser.sleep(2000);
+
+    (await element.all(by.css('.product-price'))).forEach(async (el: ElementFinder) => {
+      const price = await el.getText();
+      if (!!price) totalPrice += parseInt(price);
+    });
+
+    // element.all(by.css('.product')).each(async (item, index) => {
+    //   await browser
+    //     .actions()
+    //     .mouseMove(item.element(by.css(".product-action button")))
+    //     .click()
+    //     .perform();
+    //   const price = await item.element(by.css("p[class='product-price']")).getText();
+    //   totalPrice += parseInt(price);
+    // });
+
+    // let zIndex = 0;
+    // const items = element.all(by.css('.product'));
+    // (async function loop() {
+    //   const el: ElementFinder = items.get(zIndex);
+    //   await browser
+    //     .actions()
+    //     .mouseMove(el.element(by.css("div[class='product-action'] button")))
+    //     .click()
+    //     .perform();
+    //   const price = await el.element(by.css("p[class='product-price']")).getText();
+    //   totalPrice += parseInt(price);
+    //   zIndex += 1;
+    //   await loop();
+    // })();
+
     await browser.sleep(3000);
     console.log('totalPrice: ', totalPrice);
 
