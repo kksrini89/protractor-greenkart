b5f5f7d56066502d85abe23b529fdf22d4a08c80
srinivasan.k.k <srinivasan.k.k@kdi.kongsberg.com>
2021-05-12 21:14:51 +0530

    search veggie: success test cases are added.

--- e2e/cart.e2e.spec.ts
+++ e2e/cart.e2e.spec.ts
@@ -2,33 +2,52 @@ import { $, browser, by, element, ElementFinder } from 'protractor';
 
 import { CartPageObject } from './../po/cart.po';
 
-describe('GreenKart - Add to cart:', () => {
+describe('GreenKart:', () => {
+  const url: string = 'https://rahulshettyacademy.com/seleniumPractise/#/';
+  const veggieFullName: string = 'grape';
+  const veggiePartialName: string = 'gra';
+
   let cartPO: CartPageObject;
+
   beforeEach(() => {
     cartPO = new CartPageObject();
   });
   afterEach(() => {
     cartPO = null;
   });
-  it('Items Count Check', async () => {
+
+  const getProductNames = (productNames: string[] = []) => {
+    return [
+      ...new Set(
+        productNames
+          .map((p) => {
+            const names = p.split('-');
+            return names.length > 0 ? names[0].trim().toLowerCase() : '';
+          })
+          .filter(Boolean)
+      )
+    ];
+  };
+
+  it('Add to cart: Items Count Check', async () => {
     browser.waitForAngularEnabled(false);
     //   Navigate to 'https://rahulshettyacademy.com/seleniumPractise/#/'
-    await cartPO.navigateTo('https://rahulshettyacademy.com/seleniumPractise/#/');
+    await cartPO.navigateTo(url);
 
     await browser.sleep(1000);
-    //   Query the list items
-    const products = element(by.className('products')).all(by.css('.product'));
+    // Query the list items
     const totalProductsCount = await cartPO.products.count();
 
-    // //   Iterate each item and click on add to cart
-    // let productsPromises: any[] = [];
-    cartPO.products.each((item, index) => {
+    // To iterate each item and click on add to cart
+    cartPO.products.each((item) => {
       item.element(by.css("div[class='product-action'] button")).click().then();
     });
-    // await Promise.all(productsPromises);
 
     await browser.sleep(3000);
-    // //   compare the total items and clicked items count
+    // compare the total items and clicked items count
+
+    console.log('--------------------Output: Test Case 1----------------------');
+
     const itemsCountEl = await cartPO.itemsCountEl.getText();
     console.log('totalProductsCount', totalProductsCount);
 
@@ -36,13 +55,13 @@ describe('GreenKart - Add to cart:', () => {
     expect(totalProductsCount).toBe(parseInt(itemsCountEl));
   });
 
-  it('Price Check', async () => {
+  it('Add to cart: Price Check', async () => {
     await browser.waitForAngularEnabled(false);
-    await cartPO.navigateTo('https://rahulshettyacademy.com/seleniumPractise/#/');
+    await cartPO.navigateTo(url);
 
     // await browser.sleep(1000);
-    await browser.executeScript("document.querySelector('.products-wrapper').scrollIntoView({ behavior: 'auto' })");
-    // document.querySelector('.products-wrapper').scrollIntoView({ behavior: 'auto' });
+    await browser.executeScript('window.scrollTo(0,0)');
+    // await browser.executeScript("document.querySelector('.products-wrapper').scrollIntoView({ behavior: 'auto' })");
     //   Query the list items
     // const totalProductsCount = await cartPO.products.count();
     let totalPrice: number = 0;
@@ -51,7 +70,7 @@ describe('GreenKart - Add to cart:', () => {
       await browser.actions().mouseMove(el).click().perform();
     });
 
-    await browser.sleep(2000);
+    await browser.sleep(2500);
 
     (await element.all(by.css('.product-price'))).forEach(async (el: ElementFinder) => {
       const price = await el.getText();
@@ -84,11 +103,84 @@ describe('GreenKart - Add to cart:', () => {
     // })();
 
     await browser.sleep(3000);
+    console.log('--------------------Output: Test Case 2----------------------');
     console.log('totalPrice: ', totalPrice);
 
     //   compare the total price and sum of clicked item's price
     const addedProductsPrice = await element(by.css("div[class='cart-info'] tr:nth-child(2) td:nth-child(3) strong")).getText();
     console.log('addedProductsPrice', addedProductsPrice);
     expect(parseInt(addedProductsPrice)).toBe(totalPrice);
+  });
+
+  it('Search Veggies with Veggie Partial Name: Success', async () => {
+    await cartPO.navigateTo(url);
+    browser.waitForAngularEnabled(false);
+
+    await browser.executeScript('window.scrollTo(0,0)');
+
+    // const searchVeggie: string = 'gra';
+
+    // Quering all the veggie names
+    let productNames: string[] = [];
+    element.all(by.css('.product-name')).each(async (product) => {
+      productNames.push(await product.getText());
+    });
+    await browser.sleep(1000);
+
+    productNames = getProductNames(productNames);
+
+    console.log('--------------------Output: Test Case 3----------------------');
+    console.log(productNames);
+
+    // Filtering veggies and get veggies count
+    const matchVeggiesLength = productNames.filter((product) => product.includes(veggiePartialName))?.length;
+    console.log('matchVeggiesLength: ', matchVeggiesLength);
+
+    // Search Veggie
+    await cartPO.searchInputEl.sendKeys(veggiePartialName);
+
+    await browser.sleep(1000);
+    // Displayed filtered veggies and get veggies count
+    const shownProducts = await element(by.css('.products')).all(by.css('.product'));
+
+    console.log('shownProducts', shownProducts.length);
+
+    expect(matchVeggiesLength).toBe(shownProducts.length);
+  });
+
+  it('Search Veggies with Veggie Full Name: Success', async () => {
+    await cartPO.navigateTo(url);
+    browser.waitForAngularEnabled(false);
+
+    await browser.executeScript('window.scrollTo(0,0)');
+
+    // const searchVeggie: string = 'gra';
+
+    // Quering all the veggie names
+    let productNames: string[] = [];
+    element.all(by.css('.product-name')).each(async (product) => {
+      productNames.push(await product.getText());
+    });
+    await browser.sleep(1000);
+
+    productNames = getProductNames(productNames);
+
+    console.log('--------------------Output: Test Case 4----------------------');
+    console.log(productNames);
+
+    // Filtering veggies and get veggies count
+    const matchVeggiesLength = productNames.filter((product) => product.includes(veggieFullName))?.length;
+    console.log('matchVeggiesLength: ', matchVeggiesLength);
+
+    // Search Veggie
+    await cartPO.searchInputEl.sendKeys(veggieFullName);
+
+    await browser.sleep(1000);
+    // Displayed filtered veggies and get veggies count
+    const shownProducts = await element(by.css('.products')).all(by.css('.product'));
+
+    console.log('shownProducts', shownProducts.length);
+
+    expect(matchVeggiesLength).toBe(shownProducts.length);
   });
 });
--- po/cart.po.ts
+++ po/cart.po.ts
@@ -4,11 +4,13 @@ export class CartPageObject {
   products: ElementArrayFinder;
   addToCartButton: ElementFinder;
   itemsCountEl: ElementFinder;
+  searchInputEl: ElementFinder;
 
   constructor() {
     this.products = element(by.className('products')).all(by.css('.product'));
     this.addToCartButton = element(by.css("div[class='product-action'] button"));
     this.itemsCountEl = element(by.css("div[class='cart-info'] tr:first-child td:nth-child(3) strong"));
+    this.searchInputEl = element(by.css("input[type='search'][class='search-keyword']"));
   }
 
   async navigateTo(url: string) {
